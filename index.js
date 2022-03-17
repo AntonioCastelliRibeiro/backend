20377d857c89572ce1352918d923531c997588ac
AntonioCastelliRibeiro <60761996+AntonioCastelliRibeiro@users.noreply.github.com>
2022-03-17 15:37:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/att', (req, res) => {
+  res.send('Atualização')
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })

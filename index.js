3835a5d7b009efd9538ca03a3021f052dc190203
AntonioCastelliRibeiro <60761996+AntonioCastelliRibeiro@users.noreply.github.com>
2022-03-18 09:13:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}));
+
 app.use(express.json());
 
 const port = process.env.PORT || 3333;
